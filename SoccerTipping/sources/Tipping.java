9358d74d8805b6c40635e8b99839dec38d3d0018
Kunlun Shen <kunlun_shen@hotmail.com>
2018-09-24 19:47:13 +1000

    second game

--- SoccerTipping/sources/Tipping.java
+++ SoccerTipping/sources/Tipping.java
@@ -4,6 +4,7 @@ public class Tipping {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("China 3: USA 0");
+		System.out.println("China 3: Japan 0");
 	}
 
 }
